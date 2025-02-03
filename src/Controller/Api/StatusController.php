be5908b6b930acc4b7ab8648b7af438a6fe5a36d
Ivan <sardyco@gmail.com>
2025-02-03 09:05:39 +0100

    phpcs installed

---
+++ src/Controller/Api/StatusController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class StatusController extends AbstractController
+{
+    #[Route(path: '/status', name: 'app_status', methods: ['GET'])]
+    public function status(): JsonResponse
+    {
+        $a = 1;
+
+        return $this->json(['status' => 'ok']);
+    }
+}
---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+final class ExceptionListener
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $response = new JsonResponse([
+                'error' => $exception->getMessage()
+            ], $exception->getStatusCode());
+
+            foreach ($exception->getHeaders() as $key => $value) {
+                $response->headers->set($key, $value);
+            }
+
+            $event->setResponse($response);
+        }
+    }
+}
